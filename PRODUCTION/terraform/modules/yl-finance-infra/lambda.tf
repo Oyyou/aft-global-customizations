4c3c4ed3bdf9ba62505b72358dbca3c35ab1cbe4
Oyyou <lewin91@hotmail.com>
2025-07-15 15:32:42 +0100

    Made changes

---
+++ PRODUCTION/terraform/modules/yl-finance-infra/lambda.tf
@@ -0,0 +1,23 @@
+resource "aws_lambda_function" "loan_handler" {
+  filename         = data.archive_file.lambda.output_path
+  function_name    = "loanHandler"
+  handler          = "index.handler"
+  runtime          = "nodejs22.x"
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.loan_table.name
+    }
+  }
+
+  role = aws_iam_role.lambda_exec_role.arn
+}
+
+resource "aws_lambda_permission" "allow_api_gateway" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.loan_handler.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.loan_api.execution_arn}/*/*"
+}
