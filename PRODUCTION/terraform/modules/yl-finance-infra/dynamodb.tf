acf2dceb72386f47c28fd276d8e0bb262910f170
Oyyou <lewin91@hotmail.com>
2025-07-15 15:45:36 +0100

    Big change

--- PRODUCTION/terraform/modules/yl-finance-infra/dynamodb.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_dynamodb_table" "loan_table" {
-  name         = var.dynamodb_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "loanID"
-  attribute {
-    name = "loanID"
-    type = "S"
-  }
-}
--- PRODUCTION/terraform/modules/yl-finance-infra/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "dynamodb_table_name" {
-  description = "Name of the DynamoDB table"
-  type        = string
-  default     = "LoanTable"
-}
